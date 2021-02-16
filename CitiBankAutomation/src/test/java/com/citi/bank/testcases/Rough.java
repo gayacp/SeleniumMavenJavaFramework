5b191d83134e505db457f2373ca28e4839990c19
gayap <gayap@192.168.2.21>
2021-02-16 18:18:53 -0500

    Committing updated code

--- CitiBankAutomation/src/test/java/com/citi/bank/testcases/Rough.java
+++ CitiBankAutomation/src/test/java/com/citi/bank/testcases/Rough.java
@@ -21,15 +21,6 @@ public class Rough extends BaseTest
 	driver.get("https://www.citi.com/");
 	driver.manage().window().maximize();
 	driver.findElement(By.xpath("//*[@id=\"creditcards\"]")).click();
-////	Thread.sleep(3000);
-//	driver.findElement(By.xpath("//*[@id='viewAllCards_Link']")).click();
-//	driver.findElement(By.xpath("//*[@id='card_0']/div/div/div[2]/div/ui-card-button/button")).click();
-////	System.out.println(driver.findElement(By.xpath("//div[@class='content-inner']/../h2")).getAttribute("sup"));
-////	System.out.println(driver.findElement(By.xpath("//div[@class='content-inner']/../h2")).getText());
-//	System.out.println(config.getProperty("defaultUserName"));
-//	driver.findElement(By.xpath("//input[@name='username']")).sendKeys("efvefvwc");
-//	driver.findElement(By.xpath("//input[@id='password']")).sendKeys("shdjfsf");
-//	driver.findElement(By.xpath("//input[@id='signInBtn']")).click();
 	driver.quit();
 	}
 }
